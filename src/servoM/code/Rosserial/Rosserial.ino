916d8ab482ba79e8908f09d372d184d56790dcc0
Pripekashkin <densik554@gmail.com>
2020-05-11 12:45:51 +0300

    Rviz feedback done

--- src/servoM/code/Rosserial/Rosserial.ino
+++ src/servoM/code/Rosserial/Rosserial.ino
@@ -5,6 +5,7 @@
 #include "AX12A.h"
 #include <ros.h>
 #include <std_msgs/UInt16.h>
+#include <std_msgs/String.h>
 #include <sensor_msgs/JointState.h>
 #include <ros/time.h>
 #include <tf/transform_broadcaster.h>
@@ -15,16 +16,8 @@
 #define BaudRate      (117600)
 #define Basic_speed   (30)
 
-ros::NodeHandle nh; //node to listen
-ros::NodeHandle nh02; //node to write
 
-
-geometry_msgs::TransformStamped t;
-tf::TransformBroadcaster broadcaster;
-
-char base_link[] = "Servo";
-char child_link[] = "Horn";
-
+ros::NodeHandle nh; //node 
 
 AX12A ax12a2;
 
@@ -37,22 +30,27 @@ double theta = 0;
 
 void JointsCallback(const sensor_msgs::JointState& cmd_msg){
   ax12a.moveSpeed(0, pos, Basic_speed); //go to zero
- pos = ceil(radToDyn(cmd_msg.position[0]));
-     
+    pos = ceil(radToDyn(cmd_msg.position[0]));    
 }
 
 
-ros::Subscriber<sensor_msgs::JointState> jointsCallback("joint_states", &JointsCallback );//subscribe
+//ros::Subscriber<sensor_msgs::JointState> jointsCallback("joint_states", &JointsCallback );//subscribe
 
+sensor_msgs::JointState msg;
 
+ros::Publisher pub("joint_states", &msg);
+
+char *name[] = {"Rotate", "Rotation2"};
+float pos1[] = {1.57, 1.57};
 
 void setup()
 {
   pinMode(13, OUTPUT);
   nh.initNode();
-  nh.subscribe(jointsCallback);
-  nh02.initNode();  //init write 
-  broadcaster.init(nh02);
+  //nh.subscribe(jointsCallback);
+      nh.advertise(pub);
+
+
 
 
   Serial3.begin(117600);
@@ -62,25 +60,23 @@ void setup()
 void loop()
 {
 
-    //dtostrf(pos,6, 2, result); 
-    //nh02.loginfo(result); //echo to uart
-   // nh.spinOnce();
+      //dtostrf(pos,6, 2, result); 
+      //nh.loginfo(result); //echo to uart
+    
+
+
+
+    msg.header.stamp = nh.now();
+    msg.name = name;
+    pos1[0] = dynToRad(ax12a.readPosition(0));
+    msg.position = pos1;
+    
+    msg.name_length = 1;
+    msg.position_length = 1;
+    pub.publish(&msg);
+    nh.spinOnce();
     delay(100);
-
-
-    //t.transform.translation.z = dynToRad(ax12a.readPosition(0));
-    t.transform.translation.z = 0.049;
-    t.transform.rotation = tf::createQuaternionFromYaw(dynToRad(ax12a.readPosition(0)));
-    t.header.frame_id = base_link;
-    t.child_frame_id = child_link;
-    t.header.stamp = nh02.now();
-    broadcaster.sendTransform(t);
-    nh02.spinOnce();
-    delay(100);
-
   
-
-
 }
 
 inline float radToDyn(float position_radians) //convert radians to degree
